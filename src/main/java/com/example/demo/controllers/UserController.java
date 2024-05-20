dfeb598939fe874080399ef56b1185a0cabc9225
fearyfiremast <fearyfiremast@gmail.com>
2024-05-19 18:25:48 -0700

    Added many files and changes from video 7

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -2,28 +2,47 @@ package com.example.demo.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 import com.example.demo.models.User;
+import com.example.demo.models.UserRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserRepository userRepo;
+
     @GetMapping("/users/view")
     public String getAllUsers(Model model) {
         System.out.println("Getting all users");
-        //TODO get users from data base
-
-        List<User> userlist = new ArrayList<>();
-        userlist.add(new User("Xander", "Password", 25));
-        userlist.add(new User("Ava", "Incorrect", 14));
-        userlist.add(new User("Joshua", "Jerma", 985));
-        userlist.add(new User("Colby", "COLBS$420$", 2));
-        // End of database call
+        List<User> userlist = userRepo.findAll();
+                // End of database call
         model.addAttribute("us", userlist);
         return "users/showAll";
+    }
+    
+    @PostMapping("/users/add")
+    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
+        System.out.println("ADD USER");
+        String newName = newuser.get("name");
+        String newPassword = newuser.get("password");
+        int newsize = Integer.parseInt(newuser.get("size"));
+        userRepo.save(new User(newName, newPassword, newsize));
+
+        response.setStatus(201);
+
+        return "users/addedUser";
     }
     
 }
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,8 +1,16 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
     private String name;
     private String password;
+
     private int size;
     public User() {
     }
@@ -28,6 +36,12 @@ public class User {
     }
     public void setSize(int size) {
         this.size = size;
+    }
+    public int getUid() {
+        return uid;
+    }
+    public void setUid(int uid) {
+        this.uid = uid;
     }
     
 
